4c4ae029f8f1da97f402e35044813faa630c6410
dejanmi <milojevic.de@gmail.com>
2020-11-14 00:07:24 +0100

    advanced augmented realtiy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # parameters
-ARG REPO_NAME="<REPO_NAME_HERE>"
-ARG DESCRIPTION="<DESCRIPTION_HERE>"
-ARG MAINTAINER="<YOUR_FULL_NAME> (<YOUR_EMAIL_ADDRESS>)"
+ARG REPO_NAME="amod-2020-advanced-augmented-reality"
+ARG DESCRIPTION="Advanced augmented reality"
+ARG MAINTAINER="Dejan Milojevic (milojevic.de@gmail.com)"
 # pick an icon from: https://fontawesome.com/v4.7.0/icons/
 ARG ICON="cube"
 
--- packages/augmented_reality_apriltag/src/augmented_reality_apriltag.py
+++ packages/augmented_reality_apriltag/src/augmented_reality_apriltag.py
@@ -9,12 +9,12 @@ import rospy
 import yaml
 import sys
 from duckietown.dtros import DTROS, NodeType
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 
 import rospkg 
 
-
+from dt_apriltags import Detector
 """
 
 This is a template that can be used as a starting point for the CRA1 exercise.
@@ -38,19 +38,83 @@ class ARNode(DTROS):
         #
         #   Write your code here
         #
+        self.bridge = CvBridge()
+        self.image = None
+        self.image_height = None
+        self.image_width = None
+        self.cam_info = None
+        self.camera_info_received = False
+        self.at_detector = Detector(families='tag36h11',
+                               nthreads=1,
+                               quad_decimate=1.0,
+                               quad_sigma=0.0,
+                               refine_edges=1,
+                               decode_sharpening=0.25,
+                               debug=0)
+
+        self.sub_image_comp = rospy.Subscriber(
+            f'/{self.veh}/camera_node/image/compressed',
+            CompressedImage,
+            self.image_cb,
+            buff_size=10000000,
+            queue_size=1
+        )
+
+        self.sub_cam_info = rospy.Subscriber(f'/{self.veh}/camera_node/camera_info', CameraInfo,
+                                             self.cb_camera_info, queue_size=1)
+
+        self.pub_aug_image = rospy.Publisher(f'/{self.veh}/{node_name}/image/compressed',
+                                             CompressedImage, queue_size=10)
+
+    def image_cb(self, image_msg):
+        try:
+            image = self.readImage(image_msg)
+        except ValueError as e:
+            self.logerr('Could not decode image: %s' % e)
+            return
+
+        self.image = image
+        self.image_height = np.shape(image)[0]
+        self.image_width = np.shape(image)[1]
+
+    def cb_camera_info(self, msg):
+        if not self.camera_info_received:
+            self.cam_info = msg
+
+        self.camera_info_received = True
+
+    def detect_april_tag(self):
+        K = np.array(self.cam_info.K).reshape((3, 3))
+        camera_params = (K[0, 0], K[1, 1], K[0, 2], K[1, 2])
+        img_grey = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        tags = self.at_detector.detect(img_grey, True, camera_params, 0.065)
+        list_hom_tag = []
+        for tag in tags:
+            list_hom_tag.append(tag.homography)
+
+        return list_hom_tag
 
 
-
-    
     def projection_matrix(self, intrinsic, homography):
         """
             Write here the compuatation for the projection matrix, namely the matrix
             that maps the camera reference frame to the AprilTag reference frame.
         """
+        K = intrinsic
+        P_ext = np.linalg.inv(K) @ homography
+        r_1 = P_ext[:, 0]
+        r_1_norm = np.linalg.norm(r_1)
+        r_1 = r_1 / r_1_norm
+        r_2 = P_ext[:, 1]
+        r_2_norm = np.linalg.norm(r_2)
+        r_2 = r_2 / r_2_norm
+        r_3 = np.cross(r_1, r_2)
+        t_norm = (r_1_norm + r_2_norm)/2
+        t = P_ext[:, 2] / t_norm
+        P_ext_new = np.concatenate((r_1, r_2, r_3, t)).reshape((-1, 4), order='F')
+        P = K @ P_ext_new
 
-        #
-        # Write your code here
-        #
+        return P
 
     def readImage(self, msg_image):
         """
@@ -88,9 +152,28 @@ class ARNode(DTROS):
     def onShutdown(self):
         super(ARNode, self).onShutdown()
 
+    def run(self):
+        while not rospy.is_shutdown():
+            if self.image is None:
+                continue
+            if not self.camera_info_received:
+                continue
+
+            list_hom_tag = self.detect_april_tag()
+            if not list_hom_tag:
+                continue
+            img_rend = self.image
+            for h in list_hom_tag:
+                proj_mat = self.projection_matrix(np.array(self.cam_info.K).reshape((3, 3)), h)
+                img_rend = self.renderer.render(img_rend, proj_mat)
+
+            comp_image = self.bridge.cv2_to_compressed_imgmsg(img_rend)
+            self.pub_aug_image.publish(comp_image)
+
 
 if __name__ == '__main__':
     # Initialize the node
     camera_node = ARNode(node_name='augmented_reality_apriltag_node')
+    camera_node.run()
     # Keep it spinning to keep the node alive
     rospy.spin()
